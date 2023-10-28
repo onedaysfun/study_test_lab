0113299caf79b57b44043d4e5d985f40f8864800
onedaysfun <onedaysfun@qq.com>
2023-10-28 16:02:44 +0800

    bugfix

--- testcase/test_study.py
+++ testcase/test_study.py
@@ -1,5 +1,7 @@
-import pytest
-import request
+import time
+import logging
+from testcase.common_tools import request_tools
+
 
 class Testcase:
     @classmethod
@@ -7,9 +9,12 @@ class Testcase:
         pass
 
     def test_01(self):
-        a = 0
-        assert a == 3
+        url = "https://baike.baidu.com/api/wikihome/homecmsdata"
+        method = "get"
+        cookies = {}
+        resp = request_tools.HttpClient().set_url(url).set_method(method).send_http()
+        logging.warning("213123123")
+        assert resp.status_code == 200
 
 
-if __name__ == '__main__':
-    pytest.main(['-vs'])
+
